2d6a1c0a7075b41fee4436e07cd1882e0a5f6a1c
ctccaozhe <ctccaozhe@qq.com>
2024-02-04 16:31:47 +0800

    :wrench: 修改配置文件

--- webshell_check_cli/lib/config/config.go
+++ webshell_check_cli/lib/config/config.go
@@ -1,9 +1,8 @@
 package config
 
 var (
-	//TODO 上线后替换地址
-	Url        = "xxx"
-	ApiKey     = "xxx"
+	Url        = "https://ti.moresec.cn/"
+	ApiKey     = "13d28c00f252ddde4371c56a4e2f47ab8ab718b5f81e2f63d82f8d974cc78b07"
 	SystemType = "unknown"
 	Arch       = "unknown"
 )
